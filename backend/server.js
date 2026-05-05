edb48b51362254f158b2135989907ad5bcd4f632
Mubarek Jemal <mubarekj71@gmail.com>
2026-05-05 15:40:42 +0300

    integrate server with routes

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,15 @@
+
 const express = require('express');
 const cors = require('cors');
+
+const taskRoutess = require('./routes/taskRoutes');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use('/', taskRoutess);
 
 let tasks = [];
 
