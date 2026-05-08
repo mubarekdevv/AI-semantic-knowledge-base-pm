f9f3f8a2103d64a1366bdb2c99f865ccef134a00
Mubarek Jemal <mubarekj71@gmail.com>
2026-05-08 22:52:05 +0300

    fix in server

--- backend/server.js
+++ backend/server.js
@@ -16,34 +16,7 @@ app.use('/', taskRoutess);
 
 
 
-// Query AI
-app.post('/query', (req, res) => {
-  const question = req.body.question.toLowerCase();
 
-  if (question.includes("delay")) {
-    const result = tasks.filter(t =>
-      new Date(t.deadline) < new Date() && t.status !== "done"
-    );
-    return res.json(result);
-  }
-
-  if (question.includes("risk")) {
-    const result = tasks.filter(t =>
-      new Date(t.deadline) < new Date() && t.status !== "done"
-    );
-    return res.json({ risks: result });
-  }
-
-  if (question.includes("project")) {
-    const name = question.split("project")[1]?.trim();
-    const result = tasks.filter(t =>
-      t.project.toLowerCase().includes(name)
-    );
-    return res.json(result);
-  }
-
-  res.json({ message: "I don't understand the query" });
-});
 
 app.listen(5000, () => {
   console.log("Server running on http://localhost:5000");
